e6ef7deec83528114440ee6ae6d72ece839a8357
ajdahcode <aj.bagarra@gmail.com>
2021-09-10 21:53:36 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.print( "What is the quote? These aren't the droids you're looking for.\n" +
+                "Who said it? Obi-Wan Kenobi\n" +
+                "Obi-Wan Kenobi says, \"These aren't the droids you're looking for.\"" );
+    }
+}
